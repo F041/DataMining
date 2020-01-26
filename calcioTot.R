d0e81172e5c4700828124eb21d7746a796fc7aaa
Gabriele Giordano <contact.ggiordano@gmail.com>
2020-01-26 17:47:58 +0100

    code

--- calcioTot.R
+++ calcioTot.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/F041/Downloads")
+setwd("~/GitHub/DataMining")
 
 ### Librerie -----
 library(tidyverse)  # for data wrangling /discussione dei dati
@@ -24,16 +24,31 @@ datiRose$X<-{
   datiRose$X=stripWhitespace(datiRose$X)
 }
 datiRose<- datiRose[!grepl("Rosa",datiRose$Rosa),]
+datiRose<- datiRose[datiRose$X!="",]
 datiRose$Valore.rosa.e<-gsub(" mln â,¬", "",datiRose$Valore.rosa.e)
-datiRose$Ã..valore.di.mercato<-gsub(" mln â,¬", "",datiRose$Ã..valore.di.mercato)
+datiRose$Valore.rosa.e<-as.numeric(gsub(",", ".", datiRose$Valore.rosa.e))
+datiRose$Valore.rosa.e<-datiRose$Valore.rosa.e*1000
 
-datiRose <- datiRose %>% na.omit()
-datiRose<- separate (datiRose,datiRose$Anno,c("IA","FA"),"-")
+datiRose$Valore.rosa.e<-gsub(" mila â,¬", "",datiRose$Valore.rosa.e)
+
+
+datiRose$Ã..valore.di.mercato<-gsub(" mila â,¬", "",datiRose$Ã..valore.di.mercato)
+datiRose$Ã..valore.di.mercato<-gsub(" mln â,¬", "1000",datiRose$Ã..valore.di.mercato)
+
+colnames(datiRose)<-c("Squadra","Rosa","Età_media","Q_stranieri","valore_rosa","valore_mercato","Anno")
+datiRose$Anno<-as.character(datiRose$Anno)
+
+datiRose<-datiRose %>%
+  separate(Anno, c("IA","FA"), "-")
+
+datiRose$IA <- sub("^", "20", datiRose$IA )
+datiRose$FA <- sub("^", "20", datiRose$FA )
+
 
 
 ### DATI PRIMA PARTE -----
 {
-ca<-read.xlsx("calcio.xlsx", sheetName = "Sheet1",header = FALSE)
+ca<-read.xlsx("C:/Users/F041/Downloads/calcio.xlsx", sheetName = "Sheet1",header = FALSE)
 ca<- ca[!grepl("ROUND",ca$X1),]#levami tutte le righe che contengono ROUND(grepl=contenere)
 ca=as.data.frame(ca)
 head(ca)
@@ -87,7 +102,7 @@ ca$X5<-as.numeric(ca$X5)
 
 ### DATI SECONDA PARTE -----
 {
-datiGab<-read.xlsx("betting2019-2020to 2011-2012.xlsx", sheetName = "Sheet1", header = FALSE)
+datiGab<-read.xlsx("C:/Users/F041/Downloads/betting2019-2020to 2011-2012 (1).xlsx", sheetName = "Sheet1", header = FALSE)
 datiGab<- datiGab[!grepl("ROUND",datiGab$X1),]#levami tutte le righe che contengono ROUND(grepl=contenere)
 datiGab$X6=as.Date(datiGab$X6,format = "%d.%m.%Y");
 datiGab=as.data.frame(datiGab)
@@ -157,42 +172,102 @@ datiGab$X4<-as.numeric(datiGab$X4)
 datiGab$X5<-as.numeric(datiGab$X5)
 
 
-datiGab<-datiGab[,c(1:7,10,11,12)]
 }
 
+### FUSIONI DATI -----
 dati<-bind_rows(ca, datiGab)
+
+datiRose$IA<-as.numeric(datiRose$IA)
+datiRose$FA<-as.numeric(datiRose$FA)
+datiRose$anno<-as.numeric(datiRose$IA)
+datiRose$S1<-datiRose$Squadra
+
+
+datiRose$S1<-gsub(" fc","",datiRose$S1)
+datiRose$S1<-gsub("as ","",datiRose$S1)
+datiRose$S1<-gsub("ac ","",datiRose$S1)
+datiRose$S1<-gsub(" ssc","",datiRose$S1)
+datiRose$S1<-gsub(" afc","",datiRose$S1)
+datiRose$S1<-gsub("acf","",datiRose$S1)
+datiRose$S1<-gsub(" cfc","",datiRose$S1)
+datiRose$S1<-gsub("ss","",datiRose$S1)
+datiRose$S1<-gsub("calcio","",datiRose$S1)
+datiRose$S1<-gsub("acr","",datiRose$S1)
+datiRose$S1<-gsub("us lecce","lecce",datiRose$S1)
+datiRose$S1<-gsub("us sassuolo","sassuolo",datiRose$S1)
+datiRose$S1<-gsub("us cittã di palermo","palermo",datiRose$S1)
+datiRose$S1<-gsub("fc internazionale","inter",datiRose$S1)
+datiRose$S1<-gsub("c napoli","napoli",datiRose$S1)
+datiRose$S1<-gsub("fc meina peloro","messina",datiRose$S1)
+datiRose$S1<-gsub("atalanta bc","atalanta",datiRose$S1)
+datiRose$S1<-gsub("uc sampdoria","sampdoria",datiRose$S1)
+datiRose$S1<-gsub("treviso fbc","treviso",datiRose$S1)
+datiRose$S1<-gsub("us sauolo","sassuolo",datiRose$S1)
+datiRose$S1<-gsub("hellverona","verona",datiRose$S1)
+datiRose$S1<-gsub("chievo verona","chievo",datiRose$S1)
+datiRose$S1<-gsub("delfino pescara","pescara",datiRose$S1)
+datiRose$S1<-gsub("fc crotone","crotone",datiRose$S1)
+
+
+
+
+dati$S1<-gsub("ac ","",dati$S1)
+dati$S1<-gsub("acr","",dati$S1)
+dati$S1<-gsub("as ","",dati$S1)
+dati$S1<-gsub("ssc napoli","napoli",dati$S1)
+
+
+
+
+datiRose$valore_rosa<- as.numeric(gsub(",", ".", datiRose$valore_rosa))
+datiRose$Età_media<- as.numeric(gsub(",", ".", datiRose$Età_media))
+datiRose$Q_stranieri<- as.numeric(gsub(",", ".", datiRose$Q_stranieri))
+datiRose$Rosa<- as.numeric(gsub(",", ".", datiRose$Rosa))
+
+
+datiP<-merge(dati, datiRose, by = c("anno","S1"), no.dups = TRUE)
+
+datiRose$S2<- datiRose$S1
+
+datiP2<-merge(datiP, datiRose, by = c("anno","S2"), no.dups = FALSE)
+datiP2$Val_diff<-(datiP2$valore_rosa.x-datiP2$valore_rosa.y)
+
+datiFinali<-datiP2[,c(1:10,12:15,20:24,28)]
+
+sum(is.na(datiP$Squadra))
+
+
 ### Modello inferenziale e GAM Analisi ----
 
-glm1<-glm(ris~S1+S2+X3+X4+X5+mese+anno+S1*anno+S2*anno,data=ca)
+glm1<-glm(ris~X3+anno+S1.x*anno+S2*anno+Q_stranieri.x+valore_mercato.x
+          +valore_mercato.y+Età_media.x+Età_media.y,data=datiP2, na.action = na.omit)
 summary(glm1)
 anova(glm1)
 drop1(glm1)
 
-glm3<-glm(ris~S1+S2+X3+X4+X5+mese+anno+S1*anno+S2*anno+S1*S2,data=ca)
+glm3<-glm(ris~S1.x*anno+S2*anno+valore_mercato.x
+          +valore_mercato.y,data=datiP2)
 summary(glm3) #non serve
 
 library(gam)
-gam1<-gam(ris~s(X3)+s(X4),data=dati)
+gam1<-gam(ris~s(valore_rosa.x)+s(valore_rosa.y),data=datiP2)
+summary(gam1)
 plot(gam1)
 #suggerisce log(X3+1)
 
-##
-ca$ris2<-ifelse(ca$ris>1,1,0)
-ca$ris2sistemato<-ifelse(ca$ris2==0,1,0)
-
-
-write.csv(ca,"C:/Users/F041/Downloads/CalcioTotale.csv", row.names = FALSE)
-
-
+gam2<-gam(ris~s(X3)+s(anno)+s(mese),data=dati)
+summary(gam2)
+plot(gam2)
 
 #### Preparazione dati ------
-dati$S1<-as.factor(dati$S1)
-dati$S2<-as.factor(dati$S2)
-dati<-na.omit(dati)
-split <- createDataPartition(y=dati$ris, p = 0.7, list = FALSE)
-train <- dati[split,]
-test <- dati[-split,]
+datiFinali$S1<-as.factor(datiFinali$S1)
+datiFinali$S2<-as.factor(datiFinali$S2)
+datiFinali<-na.omit(datiFinali)
+split <- createDataPartition(y=datiFinali$ris, p = 0.7, list = FALSE)
+train <- datiFinali[split,]
+test <- datiFinali[-split,]
 train$ris<-as.factor((train$ris))
+train$S1<-as.factor((train$S1))
 levels(train$ris) <- c("Win", "Bank")
 train<-train[!train$S1=="cesena" | !train$S2=="cesena",]
 
@@ -200,22 +275,33 @@ train<-train[!train$S1=="cesena" | !train$S2=="cesena",]
 
 ####  Lasso -----
 set.seed(1234)
-grid = expand.grid(.alpha=1,.lambda=seq(0, 0.152, by = 0.001)) #non serve farlo arrivare a 1
+grid = expand.grid(.alpha=seq(0.2, 0.7, by = 0.025),.lambda=seq(0, 0.025, by = 0.0001)) #non serve farlo arrivare a 1
 Control=trainControl(method= "cv",number=10, classProbs=TRUE)
-glm_lasso=train(ris~log(X3+1)+S1*anno+S2*anno+anno, method = "glmnet", data=train,
-                trControl = Control, tuneLength=10, tuneGrid=grid, metric="Accuracy", na.action=na.exclude, 
-                cost = list(loss=loss_matr))
+glm_lasso=train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), method = "glmnet", data=train,
+                trControl = Control, tuneLength=10, tuneGrid=grid, metric="Accuracy", na.action=na.exclude)
 glm_lasso
 plot(glm_lasso)
 getTrainPerf(glm_lasso)
 plot(varImp(object=glm_lasso),main="train tuned - Variable Importance")
+
+####  Lasso 2 -----
+set.seed(1234)
+train$ck=0.8
+grid = expand.grid(.alpha=seq(0.2, 0.7, by = 0.05),.lambda=seq(0, 0.152, by = 0.001)) #non serve farlo arrivare a 1
+Control=trainControl(method= "cv",number=10, classProbs=TRUE)
+glm_lasso2=train(ris~log(X3^0.8)+anno, method = "glmnet", data=train,
+                trControl = Control, tuneLength=10, tuneGrid=grid, metric="Accuracy", na.action=na.exclude)
+glm_lasso2
+plot(glm_lasso2)
+getTrainPerf(glm_lasso2)
+plot(varImp(object=glm_lasso2),main="train tuned - Variable Importance")
 
 
 ### Albero ------
 set.seed(1)
 Grid2 = expand.grid(.cp     =seq(0,0.3,  by=0.01)) #non serve farlo arrivare a 1
 cvCtrl <- trainControl(method = "cv", number=10, classProbs = TRUE)
-rpartTuneCvA <- train(ris~S1+S2+log(X3+1)+S1*anno+S2*anno+anno, data = train, method = "rpart",
+rpartTuneCvA <- train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "rpart",
                       tuneLength = 10, na.action=na.exclude, tuneGrid=Grid2,
                       trControl = cvCtrl, metric="Accuracy")
 
@@ -228,7 +314,7 @@ plot(varImp(object=rpartTuneCvA),main="train tuned - Variable Importance")
 set.seed(1)
 Grid4 = expand.grid(.mtry   =seq(2,8,  by=2))  #by da 2 a 1, ovviamente ci metterà di più
 cvCtrl <- trainControl(method = "cv", number=10, classProbs = TRUE)
-rfTune <- train(ris~S1+S2+log(X3+1)+S1*anno+S2*anno+anno, data = train, method = "rf",
+rfTune <- train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "rf",
                 tuneLength = 2, #troppo lungho con 5
                 trControl = cvCtrl, na.action=na.exclude, tuneGrid=Grid4) # con tuneLength=5 ci mette tanto
 rfTune
@@ -237,47 +323,54 @@ plot(varImp(object=rfTune),main="train tuned - Variable Importance")
 
 ### Neural Net -----
 set.seed(2)
-Grid0 = expand.grid(.size   =seq(1,7,  by=1), .decay = seq(0.00,1,  by=0.1))
+Grid0 = expand.grid(.size   =seq(4,12,  by=1), .decay = seq(0.4,1.2,  by=0.05))
 cvCtrl <- trainControl(method = "cv", number=10, classProbs = TRUE)
-NNTune <- train(ris~S1+S2+log(X3+1)+S1*anno+S2*anno+anno, data = train, method = "nnet",
-                tuneLength = 4, tuneGrid=Grid0,
+NNTune <- train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "nnet",
+                tuneLength = 5, tuneGrid=Grid0,
                 trControl = cvCtrl, preProcess="range", na.action=na.exclude) # Non andare oltre il 6 di tuneLength 
 NNTune
+plot(NNTune)
 getTrainPerf(NNTune)
 plot(varImp(object=NNTune),main="train tuned - Variable Importance")
 
 ### Xtreme Gradient -----
 set.seed(3)
 cvCtrl <- trainControl(method = "cv", number=10, search="grid", classProbs = TRUE)
-XGBTune <- train(ris~S1+S2+log(X3+1)+S1*anno+S2*anno+anno, data = train, method = "xgbTree",
+XGBTune <- train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "xgbTree",
                  tuneLength = 4,
                  trControl = cvCtrl, metric="Accuracy", na.action=na.exclude) # Non andare oltre il 6 di tuneLength 
 XGBTune
+plot(XGBTune)
 getTrainPerf(XGBTune)
 
 ### Adaboost ------
 set.seed(6)
-cvCtrl <- trainControl(method = "cv", number=10, search="grid", classProbs = TRUE)
-AdaBTune <- train(ris~S1+S2+X3+S1*anno+S2*anno+anno, data = train, method = "AdaBoost.M1",
-                  tuneLength = 3, na.action=na.exclude, #min lenght =2
-                  trControl = cvCtrl, metric="Accuracy")
+GridAda = expand.grid(.iter   =seq(50,150,  by=50), .maxdepth = seq(1,10,  by=2), .nu= seq(0,1,  by=0.25))
+
+cvCtrl <- trainControl(method = "cv", number=10, classProbs = TRUE)
+AdaBTune <- train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "ada",
+                  tuneLength = 1, na.action=na.exclude, #min lenght =2
+                  trControl = cvCtrl, metric="Accuracy",tuneGrid=GridAda)
 
 AdaBTune
 getTrainPerf(AdaBTune)
 
+
 ### Naive Bayes ------
 set.seed(5)
-cvCtrl <- trainControl(method = "cv", number=10, search="grid", classProbs = TRUE)
-NBtune <- train(ris~S1+S2+log(X3+1)+S1*anno+S2*anno+anno, data = train, method = "nb",
-                tuneLength = 5, na.action=na.exclude,
-                trControl = cvCtrl, metric="Sens")
+gridNB<-expand.grid(.fL   =seq(0,1,  by=0.1), .adjust = seq(0,1,  by=0.1), .usekernel=c("TRUE","FALSE"))
+cvCtrl <- trainControl(method = "cv", number=10, classProbs = TRUE)
+NBtune <- train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "nb",
+                tuneLength = 10, na.action=na.exclude,
+                trControl = cvCtrl, metric="Accuracy", tuneGrid=gridNB)
 
 NBtune
+plot(NBtune)
 getTrainPerf(NBtune)
 
 ### PLS ------
 
-pls=train(ris~S1+S2+X3+S1*anno+S2*anno+anno, data = train, method = "pls", 
+pls=train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "pls", 
           trControl = cvCtrl, tuneLength=10, na.action=na.exclude)
 pls
 getTrainPerf(pls)
@@ -285,16 +378,16 @@ getTrainPerf(pls)
 ### deep boost ------
 set.seed(115)
 cvCtrl <- trainControl(method = "cv", number=10, search="grid", classProbs = TRUE)
-Deep=train(ris~S1+S2+X3+S1*anno+S2*anno+anno, data = train, method = "deepboost", 
-           trControl = cvCtrl, tuneLength=3, na.action=na.exclude)
+Deep=train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "deepboost", 
+           trControl = cvCtrl, tuneLength=3, na.action=na.exclude) #con 5 dà problemi
 Deep
 getTrainPerf(Deep)
 
 ### KNN ------
 set.seed(115)
 cvCtrl <- trainControl(method = "cv", number=10, search="grid", classProbs = TRUE)
-kknnT=train(ris~S1+S2+X3+S1*anno+S2*anno+anno, data = train, method = "kknn", 
-            trControl = cvCtrl, tuneLength=5, na.action=na.exclude)
+kknnT=train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "kknn", 
+            trControl = cvCtrl, tuneLength=10, na.action=na.exclude)
 kknnT
 getTrainPerf(kknnT)
 
@@ -303,10 +396,30 @@ getTrainPerf(kknnT)
 ### SVM ------
 set.seed(111)
 cvCtrl <- trainControl(method = "cv", number=10, search="grid", classProbs = TRUE)
-svmT=train(ris~S1+S2+X3+S1*anno+S2*anno+anno, data = train, method = "svmLinear3", 
+svmT=train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "svmLinear3", 
            trControl = cvCtrl, tuneLength=5, na.action=na.exclude)
 svmT
 getTrainPerf(svmT)
+
+## GLMNET h2o ----
+set.seed(112)
+grid = expand.grid(.alpha=seq(0.2, 0.7, by = 0.025),.lambda=seq(0, 0.025, by = 0.01)) #non serve farlo arrivare a 1
+cvCtrl <- trainControl(method = "cv", number=10, classProbs = TRUE)
+GLMNT<-train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = "glmnet_h2o", 
+           trControl = cvCtrl, tuneLength=1, na.action=na.exclude, tuneGrid=grid)
+GLMNT
+plot(GLMNT)
+getTrainPerf(GLMNT)
+
+## GPLS ----
+set.seed(112)
+cvCtrl <- trainControl(method = "cv", number=10, search="grid", classProbs = TRUE)
+plsRglmT<-train(ris~S1+S2+anno+poly(valore_rosa.x,3)+log(valore_rosa.y+1), data = train, method = 'plsRglm', 
+            trControl = cvCtrl, tuneLength=6, na.action=na.exclude)
+plsRglmT
+plot(plsRglmT)
+getTrainPerf(plsRglmT)
+
 
 ### Risultati ----
 results <- resamples(list(Lasso=glm_lasso, Tree=rpartTuneCvA, RandomForest=rfTune, 
@@ -330,6 +443,8 @@ test$p7 = predict(pls, test, "prob")[,1]
 test$p8 = predict(NBtune, test, "prob")[,1]
 test$p9 = predict(Deep, test, "prob")[,1]
 test$p10 = predict(svmT, test, "prob")[,1]
+test$pc<-ifelse(test$X3<2,1,2)
+test$gp<-predict(plsRglmT,test,"prob")[,1]
 
 
 
@@ -344,6 +459,8 @@ r6=roc(ris ~ p6, data = test)
 r7=roc(ris ~ p7, data = test)
 r8=roc(ris ~ p8, data = test)
 r9=roc(ris ~ p9, data = test)
+rc=roc(ris ~ pc, data = test)
+rgp=roc(ris ~ gp, data = test)
 
 
 plot(r1) #lasso nero
@@ -355,11 +472,13 @@ plot(r6,add=T,col="orange") #Ada
 plot(r7,add=T,col="green") #PLS
 plot(r8,add=T) #NB
 plot(r9,add=T) #deep
+plot(rc,add=T, col="red") #dcontrol
+plot(rgp,add=T, col="violet") #dcontrol
 legend("bottomright", c("lasso","Tree", "Random Forest", "NN","XGB","ADA", "PLS", "NB", "Deep"),
        text.col=c("black","red","blue","yellow","violet","orange","Green"))
 
 ### Scelta soglia migliore modello -----
-pROC_obj <- roc(test$ris,test$p1,
+pROC_obj <- roc(test$ris,test$p4,
                 smoothed = TRUE,
                 # arguments for ci
                 ci=TRUE, ci.alpha=0.9, stratified=FALSE,
@@ -367,13 +486,13 @@ pROC_obj <- roc(test$ris,test$p1,
                 plot=TRUE, auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE,
                 print.auc=TRUE, show.thres=TRUE)
 
-plot(pROC_obj, print.thres = quantile(pROC_obj$thresholds, seq(0.55,0.8,0.025))) #suggerisce un 0.474
+plot(pROC_obj, print.thres = quantile(pROC_obj$thresholds, seq(0.10,0.8,0.02))) 
 
 
 ### Risultati miglior modello, Lasso ------
-test$labelsp1<-as.factor(ifelse(test$p1>0.474,1,2))
+test$labelsp1<-as.factor(ifelse(test$p4>0.569,1,2))
 test$ris<-as.factor(test$ris)
-cm<-confusionMatrix(test$labelsp1, test$ris, positive="1") #Tante metriche, bellissimo
+cm<-confusionMatrix(test$labelsp1, test$ris, positive="1") 
 
 draw_confusion_matrix <- function(cm) {
   
